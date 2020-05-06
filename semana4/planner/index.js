0349bab6724f9310b9d930879a45198192fcc2a7
marcosoliveira <marcosroliveira@outlook.com>
2020-05-05 23:01:37 -0300

    Feito mais alguns exercícios do desafio

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,13 +1,53 @@
 
 const inputTarefa = document.getElementById("tarefa");
 
-const minhaLista = document.getElementsByTagName("ul")[0];
+const diaSemana = document.getElementById("dias");
 
 function addTarefa() {
     let item = inputTarefa.value;
+    let minhaLista = " ";
 
-    if (dias === "Segunda-feira") {
-    minhaLista.innerHTML += `<li>${item}</li>`;
-    meuInput.value = " ";
-    } 
+    if (item == "") {
+        alert("Tarefa em branco");
+        
+    } else if (diaSemana.value === "segunda") {
+        minhaLista = document.getElementById("tarefaSegunda");
+        minhaLista.innerHTML += `<li>${item}</li>`;
+        inputTarefa.value = "";
+        
+    } else if (diaSemana.value === "terca") {
+        minhaLista = document.getElementById("tarefaTerca");
+        minhaLista.innerHTML += `<li>${item}</li>`;
+        inputTarefa.value = "";
+
+    } else if (diaSemana.value === "quarta") {
+        minhaLista = document.getElementById("tarefaQuarta");
+        minhaLista.innerHTML += `<li>${item}</li>`;
+        inputTarefa.value = "";
+
+    } else if (diaSemana.value === "quinta") {
+        minhaLista = document.getElementById("tarefaQuinta");
+        minhaLista.innerHTML += `<li>${item}</li>`;
+        inputTarefa.value = "";
+
+    } else if (diaSemana.value === "sexta") {
+        minhaLista = document.getElementById("tarefaSexta");
+        minhaLista.innerHTML += `<li>${item}</li>`;
+        inputTarefa.value = "";
+
+    } else if (diaSemana.value === "sabado") {
+        minhaLista = document.getElementById("tarefaSabado");
+        minhaLista.innerHTML += `<li>${item}</li>`;
+        inputTarefa.value = "";
+
+    } else if (diaSemana.value === "domingo") {
+        minhaLista = document.getElementById("tarefaDomingo");
+        minhaLista.innerHTML += `<li>${item}</li>`;
+        inputTarefa.value = "";
+
+    }
+
 }
+
+
+
